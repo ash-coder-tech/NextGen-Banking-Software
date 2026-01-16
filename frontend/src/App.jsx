0666496591c1e726de74b54701eca1e98ae2695f
Rudra Singh <rudrasingh05@Mac.mshome.net>
2026-01-16 12:04:43 +0530

    app.jsx file updation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/transactions" element={<TransactionHistory />} /> */}
       </Routes>
     </Router>
   );
